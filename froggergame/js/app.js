7cf10ee0bbd05a7edcde126237644d0bffb07016
NaturalFractals <jcochran800@gmail.com>
2016-12-05 00:39:54 -0500

    Updated app.js
    Added player object and implemented Enemy.prototype.update function.

--- froggergame/js/app.js
+++ froggergame/js/app.js
@@ -1,26 +1,38 @@
 // Enemies our player must avoid
 //global variable
 var score = 0;
-var max_speed = 500;
-var min_speed = 200;
+var possibleX = [0, 100, 200, 300, 400];
+var possibleY = [55, 140, 225, 310];
 
-var Enemy = function() {
+// Enemies our player must avoid
+var Enemy = function(x, y) {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
+    this.speed = Math.floor((Math.random() * 400) + 200);
     this.sprite = 'images/enemy-bug.png';
     this.x = x;
     this.y = y;
-    this.speed = this.get_speed();
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    return Math.floow(Math.random() * (max_speed - min_speed + 1) + min_speed);
+    if(this.x < 500) {
+        this.x += (this.speed * dt);
+    } else {
+        this.x = -100;
+    }
+
+    //Handle collision
+    if(this.x < player.x + 30 
+        && this.x + 60 > player.x
+        && this.y + 30 > player.y
+        && this.y < player.y + 60) {
+        player.reset();
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -31,7 +43,11 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-
+var Player = function() {
+    this.sprite = 'images/char-horn-girl.png'
+    this.x = 200;
+    this.y = 400;
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
